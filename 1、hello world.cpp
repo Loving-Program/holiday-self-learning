6a79e1cdc4065ec9368428b4f82ec629e0dbcc38
Loving-Program <33521532+Loving-Program@users.noreply.github.com>
2018-01-23 17:09:56 +0800

    Add files via upload

---
+++ 1、hello world.cpp
@@ -0,0 +1,19 @@
+//初试C++ 
+#include <iostream>
+using namespace std; //使用标准命名空间
+int main()  
+{
+	cout << "hello world" << endl;
+//	return 0;
+}
+/*
+c out 输出流对象
+<<输出流 流插入运算符 
+endl换行 
+
+	cout << "hello world"; 
+	cout << endl;
+	
+	cout << "hello world" 
+		<< endl;
+*/
---
+++ 3-1birthday card.cpp
@@ -0,0 +1,20 @@
+//birthday card 1
+#include <iostream>
+using namespace std;
+int main()
+{
+	char name1[30],name2[30];
+	cin  >> name1 >> name2 ;
+	cout << "*******************" << endl;
+	cout << name1 << endl ;
+	cout << endl ;
+	cout << "  happy birthday!" << endl ;
+	cout << endl ;
+	cout << "     yours sincerely " << endl ;
+	cout << "                ";
+	cout <<  name2 <<endl ;
+	cout << "*******************" <<endl ;
+	return 0 ;
+
+}
+
